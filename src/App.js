1e9760cd7edd97c873e7d8f01a5941a665668dcf
Parikshit Hooda <phooda804@live.com>
2022-02-01 17:05:01 +0530

    added ParentOne.js and ChildOne.js Demo of avoiding unnecessary renders using props

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 // import { ArrayUseState } from "./components/ImmutableState/ArrayUseState";
 // import { ObjectUseState } from "./components/ImmutableState/ObjectUseState";
-import { Parent } from "./components/ParentChild/Parent";
+// import { Parent } from "./components/ParentChild/Parent";
 // import UseReducer from "./components/UseReducer/UseReducer";
 // import UseState from "./components/UseState/UseState";
+import { ChildOne } from "./components/Optimizations/ChildOne";
+// import { OptParentOne } from "./components/Optimizations/OptParentOne";
+import { ParentOne } from "./components/Optimizations/ParentOne";
 import "./styles.css";
 
 export default function App() {
@@ -13,7 +16,11 @@ export default function App() {
       {/* <UseReducer /> */}
       {/* <ObjectUseState /> */}
       {/* <ArrayUseState /> */}
-      <Parent />
+      {/* <Parent /> */}
+      {/* <ParentOne /> */}
+      <ParentOne>
+        <ChildOne />
+      </ParentOne>
     </div>
   );
 }
